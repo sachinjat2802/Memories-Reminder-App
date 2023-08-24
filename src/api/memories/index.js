6fbe6dac2813773b163a68ac7a714cda5abf247d
Navin3d <smnavin65@gmail.com>
2023-08-25 01:48:25 +0530

    crud done

--- src/api/memories/index.js
+++ src/api/memories/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createMemory, getAllMemories } = require("./memory-controller");
+const { createMemory, updateMemory, getAllMemories, getAMemory } = require("./memory-controller");
 const { authorizationService } = require("../../services");
 const router = express.Router();
 
 router.get("/", authorizationService.authenticateUser, getAllMemories);
+router.get("/:id", authorizationService.authenticateUser, getAMemory);
 router.post("/create", authorizationService.authenticateUser, createMemory);
+router.post("/update", authorizationService.authenticateUser, updateMemory);
 
 module.exports = router;
--- src/api/memories/memory-controller.js
+++ src/api/memories/memory-controller.js
@@ -3,13 +3,13 @@ const { validationService } = require("../../services");
 
 const createMemory = async (req, res) => {
     const { email } = req.user;
-    const { tittle, description, tags=[], dateOfEvent } = req.body;
-    if(!tittle || !description || !dateOfEvent)
+    const { tittle, description, tags = [], dateOfEvent } = req.body;
+    if (!tittle || !description || !dateOfEvent)
         return res.status(400).json({
             message: "Enter tittle, description, dateOfEvent",
             status: 0,
         });
-    if(!validationService.isValidEmail(email))
+    if (!validationService.isValidEmail(email))
         return res.status(400).json({
             message: "Enter an valid email address...",
             status: 0,
@@ -32,6 +32,50 @@ const createMemory = async (req, res) => {
     });
 }
 
+const updateMemory = async (req, res) => {
+    const { email } = req.user;
+    const { id, tittle, description, tags = [], dateOfEvent } = req.body;
+    if (!id || !tittle || !description || !dateOfEvent)
+        return res.status(400).json({
+            message: "Enter id, tittle, description, dateOfEvent",
+            status: 0,
+        });
+    if (!validationService.isValidEmail(email))
+        return res.status(400).json({
+            message: "Enter an valid email address...",
+            status: 0,
+        });
+    const { matchedCount } = await Memory.updateOne(
+        {
+            _id: id,
+            belongs_to: email,
+        },
+        {
+            $set: {
+                tittle,
+                description,
+                tags: tags.split(","),
+                event_date: dateOfEvent,
+                image: {
+                    name: req.files.file.name,
+                    data: Buffer.from(req.files.file.data),
+                    contentType: "image/jpeg",
+                }
+            }
+        }
+    );
+    if (matchedCount == 0)
+        return res.status(404).json({
+            message: "Memory does not exist...",
+            status: 0,
+            error: "Invalid memory id."
+        });
+    return res.status(201).json({
+        message: "Successfully updated Memory...",
+        status: 1,
+    });
+}
+
 const getAllMemories = async (req, res) => {
     const { email } = req.user;
     const memories = await Memory.find({ belongs_to: email });
@@ -42,7 +86,20 @@ const getAllMemories = async (req, res) => {
     });
 }
 
+const getAMemory = async (req, res) => {
+    const { email } = req.user;
+    const { id } = req.params;
+    const memories = await Memory.find({ _id: id, belongs_to: email });
+    return res.status(200).json({
+        message: "Here is your memory...",
+        status: 1,
+        data: memories,
+    });
+}
+
 module.exports = {
     createMemory,
+    updateMemory,
     getAllMemories,
+    getAMemory,
 }
