8b68f3b7bc112ac8bddab065f55c6cf3b6e2ef93
Navin3d <smnavin65@gmail.com>
2023-08-26 12:51:21 +0530

    changed DB URL and Mail working

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const mongooseConnect = require("./mongoose-config");
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB_URL: process.env.DB_URL || "mongodb://docker:mongopw@localhost:55000",
+    DB_URL: process.env.DB_URL || "mongodb+srv://myAtlasDBUser:root@myatlasclusteredu.u3lj8ji.mongodb.net/?retryWrites=true&w=majority",
     TOKEN_SECRET: process.env.TOKEN_SECRET || "hi this is memory app...",
     mongooseConnect,
 }
