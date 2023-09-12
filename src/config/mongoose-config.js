c55a21723b5e6492c1715b33d6b1ad5e2a78e155
Navin3d <smnavin65@gmail.com>
2023-09-12 22:45:35 +0530

    fix db url

--- src/config/mongoose-config.js
+++ src/config/mongoose-config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async (DB_URL) => {
     await mongoose
-        .connect(DB_URL)
+        .connect(DB_URL, {})
         .then(()=> {
             console.log("Mongoose connection is opened successfully ");
         })
