0444b6cb567cae0d304bbf4c3bcf35d250c4a932
Navin3d <smnavin65@gmail.com>
2023-10-23 11:10:44 +0530

    changed email

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,9 @@ const DB_NAME = "memories";
 module.exports = {
     HOST: process.env.HOST || "http://139.59.95.44",
     PORT: process.env.PORT || 3000,
-    MAILID: process.env.MAILID || 'dummypurposepvt@gmail.com',
-    MAIL_PASSWORD: process.env.MAIL_PASSWORD || 'zwegashxkjmcjwrp',
+    MAILID: process.env.MAILID || 'notifications@revisitro.com',
+    MAIL_USER: process.env.MAIL_USER || 'AKIA2Y7GDWJQIDAWOBWL',
+    MAIL_PASSWORD: process.env.MAIL_PASSWORD || 'BLiRGE6mV4SYkQqDPMas5tDeRyZgr1kZ2rvcIgWOHR00',
     DB_URL: process.env.DB_URL || `mongodb+srv://myAtlasDBUser:root@myatlasclusteredu.u3lj8ji.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
     TOKEN_SECRET: process.env.TOKEN_SECRET || "hi this is memory app...",
     mongooseConnect,
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,17 +1,14 @@
 const { createTransport } = require("nodemailer");
-const { MAILID, MAIL_PASSWORD } = require("../config/index");
+const { MAILID, MAIL_USER, MAIL_PASSWORD } = require("../config/index");
 
 
 const transporter = createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    secureConnection: false,
+    host: 'email-smtp.us-east-1.amazonaws.com',
+    port: 587, // Standard SMTP port (can be different based on your provider)
     auth: {
-        user: MAILID,
-        pass: MAIL_PASSWORD,
-    },
+        user: MAIL_USER,
+        pass: MAIL_PASSWORD
+    }
 });
 
 const generateOTP = () => {
@@ -28,14 +25,6 @@ const sendMail = async (to, subject, body) => {
             subject,
             text: body,
         };
-        // transporter.sendMail(mailBody, (error, info) => {
-        //     if (error) {
-        //         console.info(`Error sending mail to user ${to}`);
-        //         console.log(error);
-        //     } else {
-        //         console.info(`mail sent to user ${to}`);
-        //     }
-        // });
         await transporter.sendMail(mailBody)
     } catch (e) {
         console.log(e);
@@ -51,14 +40,6 @@ const sendOTP = async (to) => {
             subject: "Your OTP",
             text: `Your OTP to login Memory App is: ${otp}`,
         };
-        // transporter.sendMail(mailBody, (error, info) => {
-        //     if (error) {
-        //         console.info(`Error sending mail to user ${to}`);
-        //         console.log(error);
-        //     } else {
-        //         console.info(`mail sent to user ${to}`);
-        //     }
-        // });
         await transporter.sendMail(mailBody);
     } catch (e) {
         console.log(e);
