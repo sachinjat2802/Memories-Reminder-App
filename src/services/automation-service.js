1c679956306b6a72655c41bd0ee35984d3ae0e19
Sachin jat <sachinjat2802@gmail.com>
2023-11-06 10:41:31 +0530

    update custom notifications

--- src/services/automation-service.js
+++ src/services/automation-service.js
@@ -8,187 +8,188 @@ const fcm = require('./../config/firebase'); // Import the Firebase Admin SDK
 
 const date = new Date();
 
-const sendNotification = async () => {
-    let uniqueMailId = [];
-    const notTodaysEvent = await getEventExceptTodays();
-    for (const event of notTodaysEvent) {
-        if (event["notification"].length > 0) {
+const sendNotification = async (memo) => {
+    // let uniqueMailId = [];
+    // const notTodaysEvent = await getEventExceptTodays();
+    // console.log(notTodaysEvent);
+    // for (const event of notTodaysEvent) {
+    //     if (event["notification"].length > 0) {
 
-            for (const notifi of event["notification"]) {
+    //         for (const notifi of event["notification"]) {
 
-                if (event["last_notification_sent"]) {
+    //             if (event["last_notification_sent"]) {
 
-                    console.log("knkjhbj", event, notifi);
-                    const diff = calculateDateDifference(event["last_notification_sent"]);
-                    const repeat = notifi["repeat"];
-                    console.log(event["mails_count"], notifi["limit"]);
+    //                 console.log("knkjhbj", event, notifi);
+    //                 const diff = calculateDateDifference(event["last_notification_sent"]);
+    //                 const repeat = notifi["repeat"];
+    //                 console.log(event["mails_count"], notifi["limit"]);
 
-                    if (event["mails_count"] <= notifi["limit"]) {
+    //                 if (event["mails_count"] <= notifi["limit"]) {
 
-                        if (notifi["tittle"]) {
-                            if (notifi["tittle"]["tittles"]) {
-                                console.log(notifi["tittle"]["tittles"], event["tittle"]);
-                                if (notifi["tittle"]["tittles"].indexOf(event["tittle"]) > -1 && notifi["tittle"]["enabled"]) {
-                                    await sendEventMail(event);
-                                }
-                            }
-                        }
+    //                     if (notifi["tittle"]) {
+    //                         if (notifi["tittle"]["tittles"]) {
+    //                             console.log(notifi["tittle"]["tittles"], event["tittle"]);
+    //                             if (notifi["tittle"]["tittles"].indexOf(event["tittle"]) > -1 && notifi["tittle"]["enabled"]) {
+    //                                 await sendEventMail(event);
+    //                             }
+    //                         }
+    //                     }
 
-                        if (notifi["description"]) {
-                            if (notifi["description"]["descriptions"]) {
-                                console.log(notifi["description"]["descriptions"], event["description"]);
-                                if (notifi["description"]["descriptions"].indexOf(event["description"]) > -1 && notifi["description"]["enabled"]) {
-                                    await sendEventMail(event);
-                                }
-                            }
-                        }
+    //                     if (notifi["description"]) {
+    //                         if (notifi["description"]["descriptions"]) {
+    //                             console.log(notifi["description"]["descriptions"], event["description"]);
+    //                             if (notifi["description"]["descriptions"].indexOf(event["description"]) > -1 && notifi["description"]["enabled"]) {
+    //                                 await sendEventMail(event);
+    //                             }
+    //                         }
+    //                     }
 
-                        if (notifi["image"]) {
-                            if (notifi["image"]["enabled"]) {
-                                if (notifi["image"]["status"] == "Has images") {
-                                    if (event["image"] && event["image"].length > 0) {
-                                        console.log("====>", notifi["image"], event["image"].length);
-                                        await sendEventMail(event);
-                                    }
-                                }
-                                if (notifi["image"]["status"] == "Doesnt have images") {
-                                    if (event["image"] && event["image"].length == 0) {
-                                        console.log("jnjknkjnkjhjhj", notifi["image"], event);
-                                        await sendEventMail(event);
-                                    }
-                                }
-                            }
-                        }
+    //                     if (notifi["image"]) {
+    //                         if (notifi["image"]["enabled"]) {
+    //                             if (notifi["image"]["status"] == "Has images") {
+    //                                 if (event["image"] && event["image"].length > 0) {
+    //                                     console.log("====>", notifi["image"], event["image"].length);
+    //                                     await sendEventMail(event);
+    //                                 }
+    //                             }
+    //                             if (notifi["image"]["status"] == "Doesnt have images") {
+    //                                 if (event["image"] && event["image"].length == 0) {
+    //                                     console.log("jnjknkjnkjhjhj", notifi["image"], event);
+    //                                     await sendEventMail(event);
+    //                                 }
+    //                             }
+    //                         }
+    //                     }
 
-                        if (notifi["tag"]) {
-                            if (notifi["tag"]["tags"] && notifi["tag"]["enabled"]) {
-                                console.log(notifi["tag"]["tags"], event["tags"]);
-                                if (notifi["tag"]["filter_match"] == "Has all of") {
-                                    if (notifi["tag"]["tags"][0].every(item => event["tags"].includes(item))) {
-                                        await sendEventMail(event);
-                                    }
-                                }
+    //                     if (notifi["tag"]) {
+    //                         if (notifi["tag"]["tags"] && notifi["tag"]["enabled"]) {
+    //                             console.log(notifi["tag"]["tags"], event["tags"]);
+    //                             if (notifi["tag"]["filter_match"] == "Has all of") {
+    //                                 if (notifi["tag"]["tags"][0].every(item => event["tags"].includes(item))) {
+    //                                     await sendEventMail(event);
+    //                                 }
+    //                             }
 
-                                if (notifi["tag"]["filter_match"] == "Has exactly") {
-                                    const notificationTags = notifi["tags"];
-                                    console.log("mhghggh", notificationTags[0]);
-                                    const conditionMet = arraysHaveExactMatch(notificationTags[0], event["tags"]);
-                                    if (conditionMet) {
-                                        await sendEventMail(event);
-                                    }
-                                }
+    //                             if (notifi["tag"]["filter_match"] == "Has exactly") {
+    //                                 const notificationTags = notifi["tags"];
+    //                                 console.log("mhghggh", notificationTags[0]);
+    //                                 const conditionMet = arraysHaveExactMatch(notificationTags[0], event["tags"]);
+    //                                 if (conditionMet) {
+    //                                     await sendEventMail(event);
+    //                                 }
+    //                             }
 
-                                if (notifi["tag"]["filter_match"] == "Has any of") {
-                                    const notificationTags = notifi["tags"];
-                                    console.log("mhghggh", notificationTags[0]);
-                                    const conditionMet = arraysHasAnyOf(notificationTags[0], event["tags"]);
-                                    if (conditionMet) {
-                                        await sendEventMail(event);
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        // if (diff >= (repeat - 1)) {
-                        console.log("diff ", diff, "repeat ", repeat);
-                        if ((diff + 1) == repeat && notifi["limit"] > 0) {
-                            await Memory.findOneAndUpdate(
-                                {
-                                    _id: event["_id"],
-                                },
-                                {
-                                    $set: {
-                                        mails_count: 0,
-                                    }
-                                }
-                            );
-                        }
-                    }
-                } else {
-                    await sendEventMail(event);
-                }
-            }
-        } else {
-            const returnV = await sendIfNotSentToday(event);
-            console.log(returnV);
-        }
-    }
+    //                             if (notifi["tag"]["filter_match"] == "Has any of") {
+    //                                 const notificationTags = notifi["tags"];
+    //                                 console.log("mhghggh", notificationTags[0]);
+    //                                 const conditionMet = arraysHasAnyOf(notificationTags[0], event["tags"]);
+    //                                 if (conditionMet) {
+    //                                     await sendEventMail(event);
+    //                                 }
+    //                             }
+    //                         }
+    //                     }
+    //                 } else {
+    //                     // if (diff >= (repeat - 1)) {
+    //                     console.log("diff ", diff, "repeat ", repeat);
+    //                     if ((diff + 1) == repeat && notifi["limit"] > 0) {
+    //                         await Memory.findOneAndUpdate(
+    //                             {
+    //                                 _id: event["_id"],
+    //                             },
+    //                             {
+    //                                 $set: {
+    //                                     mails_count: 0,
+    //                                 }
+    //                             }
+    //                         );
+    //                     }
+    //                 }
+    //             } else {
+    //                 await sendEventMail(event);
+    //             }
+    //         }
+    //     } else {
+    //         const returnV = await sendIfNotSentToday(event);
+    //         console.log(returnV);
+    //     }
+    // }
 
-    const todaysEvent = await getTodaysEvent();
-    for (const event of todaysEvent) {
-        for (let notifi of event["notification"]) {
-            if (notifi["tittle"]) {
-                if (notifi["tittle"]["tittles"]) {
-                    console.log(notifi["tittle"]["tittles"], event["tittle"]);
-                    if (notifi["tittle"]["tittles"].indexOf(event["tittle"]) > -1 && notifi["tittle"]["enabled"]) {
-                        await sendEventMail(event);
-                    }
-                }
-            }
+    // const todaysEvent = await getTodaysEvent();
+    // for (const event of todaysEvent) {
+    //     for (let notifi of event["notification"]) {
+    //         if (notifi["tittle"]) {
+    //             if (notifi["tittle"]["tittles"]) {
+    //                 console.log(notifi["tittle"]["tittles"], event["tittle"]);
+    //                 if (notifi["tittle"]["tittles"].indexOf(event["tittle"]) > -1 && notifi["tittle"]["enabled"]) {
+    //                     await sendEventMail(event);
+    //                 }
+    //             }
+    //         }
 
-            if (notifi["description"]) {
-                if (notifi["description"]["descriptions"]) {
-                    console.log(notifi["description"]["descriptions"], event["description"]);
-                    if (notifi["description"]["descriptions"].indexOf(event["description"]) > -1 && notifi["description"]["enabled"]) {
-                        await sendEventMail(event);
-                    }
-                }
-            }
+    //         if (notifi["description"]) {
+    //             if (notifi["description"]["descriptions"]) {
+    //                 console.log(notifi["description"]["descriptions"], event["description"]);
+    //                 if (notifi["description"]["descriptions"].indexOf(event["description"]) > -1 && notifi["description"]["enabled"]) {
+    //                     await sendEventMail(event);
+    //                 }
+    //             }
+    //         }
 
-            if (notifi["image"]) {
-                if (notifi["image"]["enabled"]) {
-                    if (notifi["image"]["status"] == "Has images") {
-                        if (event["image"] && event["image"].length > 0) {
-                            console.log("====>", notifi["image"], event["image"].length);
-                            await sendEventMail(event);
-                        }
-                    }
-                    if (notifi["image"]["status"] == "Doesnt have images") {
-                        if (event["image"] && event["image"].length == 0) {
-                            console.log("jnjknkjnkjhjhj", notifi["image"], event);
-                            await sendEventMail(event);
-                        }
-                    }
-                }
-            }
+    //         if (notifi["image"]) {
+    //             if (notifi["image"]["enabled"]) {
+    //                 if (notifi["image"]["status"] == "Has images") {
+    //                     if (event["image"] && event["image"].length > 0) {
+    //                         console.log("====>", notifi["image"], event["image"].length);
+    //                         await sendEventMail(event);
+    //                     }
+    //                 }
+    //                 if (notifi["image"]["status"] == "Doesnt have images") {
+    //                     if (event["image"] && event["image"].length == 0) {
+    //                         console.log("jnjknkjnkjhjhj", notifi["image"], event);
+    //                         await sendEventMail(event);
+    //                     }
+    //                 }
+    //             }
+    //         }
 
-            if (notifi["tag"]) {
-                if (notifi["tag"]["tags"] && notifi["tag"]["enabled"]) {
-                    console.log("==>", event["tags"]);
-                    if (notifi["tag"]["filter_match"] == "Has all of") {
-                        if (notifi["tag"]["tags"][0].every(item => event["tags"].includes(item))) {
-                            await sendEventMail(event);
-                        }
-                    }
+    //         if (notifi["tag"]) {
+    //             if (notifi["tag"]["tags"] && notifi["tag"]["enabled"]) {
+    //                 console.log("==>", event["tags"]);
+    //                 if (notifi["tag"]["filter_match"] == "Has all of") {
+    //                     if (notifi["tag"]["tags"][0].every(item => event["tags"].includes(item))) {
+    //                         await sendEventMail(event);
+    //                     }
+    //                 }
 
-                    if (notifi["tag"]["filter_match"] == "Has exactly") {
-                        const notificationTags = notifi["tags"];
-                        console.log("mhghggh", notificationTags[0]);
-                        const conditionMet = arraysHaveExactMatch(notificationTags[0], event["tags"]);
-                        if (conditionMet) {
-                            await sendEventMail(event);
-                        }
-                    }
+    //                 if (notifi["tag"]["filter_match"] == "Has exactly") {
+    //                     const notificationTags = notifi["tags"];
+    //                     console.log("mhghggh", notificationTags[0]);
+    //                     const conditionMet = arraysHaveExactMatch(notificationTags[0], event["tags"]);
+    //                     if (conditionMet) {
+    //                         await sendEventMail(event);
+    //                     }
+    //                 }
                     
-                    if (notifi["tag"]["filter_match"] == "Has any of") {
-                        const notificationTags = notifi["tags"];
-                        const conditionMet = arraysHasAnyOf(notificationTags[0], event["tags"]);
-                        console.log("conditionMet 2 ", conditionMet);
-                        if (conditionMet) {
-                            await sendEventMail(event);
-                        }
-                    }
+    //                 if (notifi["tag"]["filter_match"] == "Has any of") {
+    //                     const notificationTags = notifi["tags"];
+    //                     const conditionMet = arraysHasAnyOf(notificationTags[0], event["tags"]);
+    //                     console.log("conditionMet 2 ", conditionMet);
+    //                     if (conditionMet) {
+    //                         await sendEventMail(event);
+    //                     }
+    //                 }
 
-                    if (notifi["tag"]["filter_match"] == "Untagged") {
-                        const conditionMet = event["tags"].length == 0;
-                        if (conditionMet) {
-                            await sendEventMail(event);
-                        }
-                    }
-                }
-            }
-        };
-    }
+    //                 if (notifi["tag"]["filter_match"] == "Untagged") {
+    //                     const conditionMet = event["tags"].length == 0;
+    //                     if (conditionMet) {
+    //                         await sendEventMail(event);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     };
+    // }
 }
 
 const sendIfNotSentToday = async (memory) => {
